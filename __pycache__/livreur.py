30dc844bfc7aefc824a0838e3868aea7515c352f
LEM-ASSIYA <143855548+LEM-ASSIYA@users.noreply.github.com>
2024-10-15 14:46:10 +0100

    livreur

--- __pycache__/livreur.py
+++ __pycache__/livreur.py
@@ -1,1 +1,8 @@
+class Livreur:
+    def __init__(self, id, nom, telephone):
+        self.id = id
+        self.nom = nom
+        self.telephone = telephone
+        self.commande_en_livraison = None
 
+   
