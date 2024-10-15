c614aa07de25cc3ceb4319dde5d78c9fc7dbe3ef
LEM-ASSIYA <143855548+LEM-ASSIYA@users.noreply.github.com>
2024-10-15 14:47:31 +0100

    livreur

--- __pycache__/livreur.py
+++ __pycache__/livreur.py
@@ -5,4 +5,12 @@ class Livreur:
         self.telephone = telephone
         self.commande_en_livraison = None
 
-   
+    def recevoir_notification(self, commande):
+        """Recevoir une notification du serveur pour une commande prête à être livrée."""
+        if commande["statut"] == "Prêt":
+            self.commande_en_livraison = commande
+            print(f"Livreur {self.nom} a reçu la notification pour la commande {commande['id']} à livrer.")
+        else:
+            print(f"Aucune commande prête pour livraison.")
+
+
