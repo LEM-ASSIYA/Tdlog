fefc5c510640baea6daad0df813d7dd91cd84b19
LEM-ASSIYA <143855548+LEM-ASSIYA@users.noreply.github.com>
2025-01-05 20:11:20 +0100

    Update utilisateur.py

--- utilisateur.py
+++ utilisateur.py
@@ -1,12 +1,12 @@
-class Utilisateur:
+from abc import ABC, abstractmethod
+import tkinter as tk
+from tkinter import messagebox
+
+# ✅ Classe abstraite Utilisateur
+class Utilisateur(ABC):
     def __init__(self, id, nom, email, mot_de_passe):
         self.id = id
         self.nom = nom
         self.email = email
         self.mot_de_passe = mot_de_passe
-
-    def se_connecter(self):
-        print(f"{self.nom} connecté.")
-
-    def se_deconnecter(self):
-        print(f"{self.nom} déconnecté.")
+        self.logged_in = False
